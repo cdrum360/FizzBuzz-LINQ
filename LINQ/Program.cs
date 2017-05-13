71caab8892a6f915bbd72426ee0bc74c64ad159b
Charles W. Drummond Jr <cdrum360@live.com>
2017-05-13 14:43:49 -0400

    Add project files.

---
+++ LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable.Range(1, 100)
+                .ToList()
+                .ForEach(i => Console.WriteLine((i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 5 == 0) ? "Buzz" : (i % 3 == 0) ? "Fizz" : i.ToString()));
+
+            Console.ReadLine();
+        }
+    }
+}
